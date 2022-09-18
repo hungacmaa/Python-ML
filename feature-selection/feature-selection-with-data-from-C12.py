882684c184726613196328e1d700cd2d225b3ba7
hungnguyen <hungkojno1@gmail.com>
2022-09-18 23:18:06 +0700

    ap dung fs cho c12

--- feature-selection/feature-selection-with-data-from-C12.py
+++ feature-selection/feature-selection-with-data-from-C12.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 17 14:37:38 2022
+Created on Sun Sep 18 22:58:33 2022
 
 @author: HungNguyen
 """
 
-import pandas as pd
 import numpy as np
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
-data = pd.read_csv("train.csv")
-X = data.iloc[:, 0:20] #independent columns
-y = data.iloc[:, -1] #output
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# bestfeatures = SelectKBest(score_func=chi2, k=10)
-# fit = bestfeatures.fit(X, y)
-# dfscores = pd.DataFrame(fit.scores_)
+data = pd.read_csv("../diabetes.csv")
+X = data.iloc[:, 0:-1]
+y = data.iloc[:, -1]
+
+# from sklearn.feature_selection import SelectKBest
+# from sklearn.feature_selection import chi2
+
+# cf = SelectKBest(score_func=chi2, k=4)
+# fit = cf.fit(X, y)
 # dfcolumns = pd.DataFrame(X.columns)
+# dfscores = pd.DataFrame(fit.scores_)
 # #concat two dataframes for better visualization 
 # featureScores = pd.concat([dfcolumns,dfscores],axis=1)
 # featureScores.columns = ['Specs','Score']  #naming the dataframe columns
-# print(featureScores.nlargest(10,'Score'))  #print 10 best features
+# print(featureScores.nlargest(4,'Score'))  #print 10 best features
 
 # from sklearn.ensemble import ExtraTreesClassifier
 # import matplotlib.pyplot as plt
@@ -29,7 +32,7 @@ y = data.iloc[:, -1] #output
 # print(model.feature_importances_) #use inbuilt class feature_importances of tree based classifiers
 # #plot graph of feature importances for better visualization
 # feat_importances = pd.Series(model.feature_importances_, index=X.columns)
-# feat_importances.nlargest(10).plot(kind='barh')
+# feat_importances.nlargest(5).plot(kind='barh')
 # plt.show()
 
 import matplotlib.pyplot as plt
@@ -39,6 +42,6 @@ import seaborn as sns
 #get correlations of each features in dataset
 corrmat = data.corr()
 top_corr_features = corrmat.index
-plt.figure(figsize=(20,20))
+plt.figure(figsize=(10,10))
 #plot heat map
 g=sns.heatmap(data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
