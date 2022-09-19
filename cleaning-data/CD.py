1a52224930d4a769c145ee2420d5a43771df1f71
hungnguyen <hungkojno1@gmail.com>
2022-09-19 11:27:40 +0700

    cleaning data

---
+++ cleaning-data/CD.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 19 00:04:55 2022
+
+@author: HungNguyen
+"""
+
+# import libraries
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('../../datahome/cleanning-data/train.csv')
+df.shape
+
+# select numerical columns
+df_numeric = df.select_dtypes(include=[np.number])
+numeric_cols = df_numeric.columns.values
+# select non-numeric columns
+df_non_numeric = df.select_dtypes(exclude=[np.number])
+non_numeric_cols = df_non_numeric.columns.values
+
+
+# % of values missing in each column
+values_list = list()
+cols_list = list()
+for col in df.columns:
+    pct_missing = np.mean(df[col].isnull())*100
+    cols_list.append(col)
+    values_list.append(pct_missing)
+pct_missing_df = pd.DataFrame()
+pct_missing_df['col'] = cols_list
+pct_missing_df['pct_missing'] = values_list
+pct_missing_df.loc[pct_missing_df.pct_missing > 0].plot(kind='bar', figsize=(12,8))
+plt.show()
+
+less_missing_values_cols_list = list(pct_missing_df.loc[(pct_missing_df.pct_missing < 0.5) & (pct_missing_df.pct_missing > 0), 'col'].values)
+df.dropna(subset=less_missing_values_cols_list, inplace=True)
+
+# dropping columns with more than 40% null values
+_40_pct_missing_cols_list = list(pct_missing_df.loc[pct_missing_df.pct_missing > 40, 'col'].values)
+df.drop(columns=_40_pct_missing_cols_list, inplace=True)
+
+df_numeric = df.select_dtypes(include=[np.number])
+numeric_cols = df_numeric.columns.values
+for col in numeric_cols:
+    missing = df[col].isnull()
+    num_missing = np.sum(missing)
+    if num_missing > 0:  # impute values only for columns that have missing values
+        med = df[col].median() #impute with the median
+        df[col] = df[col].fillna(med)
+
+df_non_numeric = df.select_dtypes(exclude=[np.number])
+non_numeric_cols = df_non_numeric.columns.values
+for col in non_numeric_cols:
+    missing = df[col].isnull()
+    num_missing = np.sum(missing)
+    if num_missing > 0:  # impute values only for columns that have missing values
+        mod = df[col].describe()['top'] # impute with the most frequently occuring value
+        df[col] = df[col].fillna(mod)
+
+df.isnull().sum().sum()
+
+df.life_sq.describe()
+
+df.life_sq.plot(kind='box', figsize=(12, 8))
+plt.show()
+
+# removing the outlier value in life_sq column
+df = df.loc[df.life_sq < 7478]
+
+# dropping duplicates by considering all columns other than ID
+cols_other_than_id = list(df.columns)[1:]
+df.drop_duplicates(subset=cols_other_than_id, inplace=True)
+
+# converting timestamp to datetime format
+df['timestamp'] = pd.to_datetime(df.timestamp, format='%Y-%m-%d')
