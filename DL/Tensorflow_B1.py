bcc2283b868aa98c38da92774f2a6a4b7778517a
hungnguyen <hungkojno1@gmail.com>
2022-10-23 22:02:53 +0700

    add some jupiter notebook file

--- DL/Tensorflow_B1.py
+++ DL/Tensorflow_B1.py
@@ -87,3 +87,53 @@ with tf.Session() as sess:
     x_data = 5
     result = sess.run(f1,feed_dict={x1:x_data}) # cách truyền giá trị cho placeholder
 print(result)
+
+a = tf.constant([[1, 2, 3], 
+                 [4, 5, 6]], 
+                tf.float32)
+b = tf.constant([[1, 2, 3, 4], 
+                 [5, 6, 7, 8], 
+                 [9, 10, 11, 12]], 
+                tf.float32)
+with tf.Session() as sess:
+    print(sess.run(tf.matmul(a, b)))
+
+
+# BAI 1
+data = [10, 2, 1, 0.5, 0, -0.5, -1., -2., -10.]
+
+# tanh and sigmoid
+E = tf.nn.tanh(data)
+print(sess.run(E))
+print(sess.run(tf.nn.sigmoid(data)))
+
+# ReLU
+R = tf.nn.relu6(data)
+print(sess.run(R))
+
+R = tf.nn.relu(data)
+print(sess.run(R))
+
+# BAI 3
+#Import required packages
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+# Getting the data ready
+# Generate train dummy data for 1000 Students and dummy test for 500
+#Columns :Age, Hours of Study &Avg Previous test scores
+np.random.seed(2018) #Setting seed for reproducibility
+train_data, test_data = np.random.random((1000, 3)), np.random.random((500, 3))
+#Generate dummy results for 1000 students : Whether Passed (1) or Failed (0)
+labels = np.random.randint(2, size=(1000, 1))
+#Defining the model structure with the required layers, # of neurons, activation function and optimizers
+model = Sequential()
+model.add(Dense(5, input_dim=3, activation='relu')) # layer 1 dung 5 neuron, activation func dung relu (0-10)
+model.add(Dense(4, activation='relu')) # layer 2 dung 4 neuron, activation func dung relu (0-10)
+model.add(Dense(1, activation='sigmoid')) # layer cuoi, dau ra tra ve 1 nhan (1 neuron), af dung sigmoid (0-1)
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#Train the model and make predictions
+model.fit(train_data, labels, epochs=10, batch_size=32)
+#Make predictions from the trained model
+predictions = model.predict(test_data)
+
