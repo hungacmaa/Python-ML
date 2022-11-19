251ff359bd26bb17ff6683c47189da84efb17423
Hung Nguyen <hungkojno1@gmail.com>
2022-11-20 00:00:11 +0700

    xong chuong 6 - linear regression

--- Tieu-luan-cuoi-ky/Chuong-1-ML-Co-Ban/Xu-Ly-Du-Lieu/Features-Selection/Features-Selection.py
+++ Tieu-luan-cuoi-ky/Chuong-1-ML-Co-Ban/Xu-Ly-Du-Lieu/Features-Selection/Features-Selection.py
@@ -1,3 +1,10 @@
 #----------------- F E A T U R E S       S E L E C T I O N ----------------------
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
- 
+data = pd.read_csv("diabetes.csv")
+print(data)
+print(data.columns)
+print(data.corr())
+
